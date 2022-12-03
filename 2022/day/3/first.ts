809daaf135adfcd3347bf6eeee49bea0c5c8b5b9
benNek <benasnek@gmail.com>
2022-12-03 15:01:21 +0100

    Complete day 3

---
+++ 2022/day/3/first.ts
@@ -0,0 +1,36 @@
+import * as path from "https://deno.land/std/path/mod.ts";
+import getLines from "../../../helpers/getLines.ts";
+
+const INPUT_FILE = "./input.txt";
+const lines = await getLines(path.dirname(path.fromFileUrl(import.meta.url)), INPUT_FILE);
+
+function calculateItemTypeScore(char: string): number {
+  // Big A starts at 65, while small one starts at 97
+  if (char.toUpperCase() === char) {
+    return char.charCodeAt(0) - 64 + 26;
+  } else {
+    return char.charCodeAt(0) - 96;
+  }
+}
+
+let sum = 0;
+for await (const line of lines) {
+  const firstCompartment = line.slice(0, line.length / 2);
+  const secondCompartment = line.slice(line.length / 2);
+
+  const firstCompartmentMap = {};
+  const duplicatesMap = {};
+  for (let i = 0; i < firstCompartment.length; i++) {
+    firstCompartmentMap[firstCompartment[i]] = true;
+  }
+
+  for (let i = 0; i < secondCompartment.length; i++) {
+    const itemType = secondCompartment[i];
+    if (firstCompartmentMap[itemType] && !duplicatesMap[itemType]) {
+      sum += calculateItemTypeScore(itemType);
+      duplicatesMap[itemType] = true;
+    }
+  }
+}
+
+console.log(sum);
---
+++ 2022/day/3/second.ts
@@ -0,0 +1,55 @@
+import * as path from "https://deno.land/std/path/mod.ts";
+import getLines from "../../../helpers/getLines.ts";
+
+const INPUT_FILE = "./input.txt";
+const lines = await getLines(path.dirname(path.fromFileUrl(import.meta.url)), INPUT_FILE);
+
+function calculateItemTypeScore(char: string): number {
+  // Big A starts at 65, while small one starts at 97
+  if (char.toUpperCase() === char) {
+    return char.charCodeAt(0) - 64 + 26;
+  } else {
+    return char.charCodeAt(0) - 96;
+  }
+}
+
+let sum = 0;
+let elfId = 1;
+let elvesItems = {};
+for await (const line of lines) {
+  // for first elf, fill his items,
+  // but for all others, only include items that were already in the previous one
+  if (Object.keys(elvesItems).length === 0) {
+    let items = {};
+    for (let i = 0; i < line.length; i++) {
+      items[line[i]] = true;
+    }
+    elvesItems[elfId] = items;
+  } else {
+    let items = {};
+    const previousElfItems = elvesItems[elfId - 1];
+    for (let i = 0; i < line.length; i++) {
+     if (previousElfItems[line[i]]) {
+        items[line[i]] = true;
+     } 
+    }
+    elvesItems[elfId] = items;
+  }
+
+  if (elfId === 3) {
+    const possibleBadges = elvesItems[elfId]
+    if (Object.keys(possibleBadges).length !== 1) {
+      throw Error(`More than one item type (${Object.keys(elvesItems[elfId]).join(',')}) for elf group`);
+    }
+
+    let badge = Object.keys(possibleBadges)[0];
+    sum += calculateItemTypeScore(badge);
+
+    elfId = 1;
+    elvesItems = {};
+  } else {
+    elfId++;
+  }
+}
+
+console.log(sum);
