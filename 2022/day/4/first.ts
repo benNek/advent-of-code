c42593ae1f9606d91458bec7b713a43e43970995
benNek <benasnek@gmail.com>
2022-12-04 12:43:24 +0100

    Finish day 4

---
+++ 2022/day/4/first.ts
@@ -0,0 +1,23 @@
+import * as path from "https://deno.land/std/path/mod.ts";
+import getLines from "../../../helpers/getLines.ts";
+
+const INPUT_FILE = "./input.txt";
+const lines = await getLines(path.dirname(path.fromFileUrl(import.meta.url)), INPUT_FILE);
+
+let totallyContained = 0;
+for await (const line of lines) {
+  const firstSection = line.split(',')[0];
+  const firstMin = parseInt(firstSection.split('-')[0], 10);
+  const firstMax = parseInt(firstSection.split('-')[1], 10);
+
+  const secondSection = line.split(',')[1];
+  const secondMin = parseInt(secondSection.split('-')[0], 10);
+  const secondMax = parseInt(secondSection.split('-')[1], 10);
+  if (firstMin >= secondMin && firstMax <= secondMax) {
+    totallyContained++;
+  } else if (secondMin >= firstMin && secondMax <= firstMax) {
+    totallyContained++;
+  }
+}
+
+console.log(totallyContained);
---
+++ 2022/day/4/second.ts
@@ -0,0 +1,36 @@
+import * as path from "https://deno.land/std/path/mod.ts";
+import getLines from "../../../helpers/getLines.ts";
+
+const INPUT_FILE = "./input.txt";
+const lines = await getLines(path.dirname(path.fromFileUrl(import.meta.url)), INPUT_FILE);
+
+function isOverlapping(a1: number, a2: number, b1: number, b2: number): boolean {
+  // probably overcomplicating and writing too many cases but if it works - it works
+  if (a1 <= b1 && b1 <= a2 && a2 <= b2) {
+    return true;
+  } else if (a1 <= b1 && b2 <= a2) {
+    return true;
+  } else if (b1 <= a1 && a1 <= b2 && b2 <= a2) {
+    return true;
+  } else if (b1 <= a1 && a2 <= b2) {
+    return true;
+  }
+  return false;
+}
+
+let overlap = 0;
+for await (const line of lines) {
+  const firstSection = line.split(',')[0];
+  const firstMin = parseInt(firstSection.split('-')[0], 10);
+  const firstMax = parseInt(firstSection.split('-')[1], 10);
+
+  const secondSection = line.split(',')[1];
+  const secondMin = parseInt(secondSection.split('-')[0], 10);
+  const secondMax = parseInt(secondSection.split('-')[1], 10);
+
+  if (isOverlapping(firstMin, firstMax, secondMin, secondMax)) {
+    overlap++;
+  }
+}
+
+console.log(overlap);
