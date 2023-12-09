698f3689ecca025a649061e24067d5fc12bb0c67
benNek <benasnek@gmail.com>
2023-12-09 09:04:52 +0100

    Solve day 9

---
+++ 2023/day/9/first.ts
@@ -0,0 +1,39 @@
+import getLines from "../../../helpers/readFile";
+
+const lines = await getLines();
+
+let sum = 0;
+for (const line of lines) {
+  const numbers = line.split(" ").map((x) => Number.parseInt(x, 10));
+  sum += exterpolate(numbers);
+}
+console.log(sum);
+
+function exterpolate(numbers: number[]) {
+  const sequences: number[][] = [numbers];
+
+  let allZeroes = false;
+  while (!allZeroes) {
+    const seq = sequences.at(-1)!;
+    const newSeq = generateSequence(seq);
+    sequences.push(newSeq);
+    allZeroes = newSeq.every((x) => x === 0);
+  }
+
+  let sum = 0;
+  let previous = 0;
+  for (let i = sequences.length - 2; i >= 0; i--) {
+    const num = previous + sequences[i].at(-2)!;
+    previous = num;
+    sum += num;
+  }
+  return sum;
+}
+
+function generateSequence(numbers: number[]): number[] {
+  const seq: number[] = [];
+  for (let i = 1; i < numbers.length; i++) {
+    seq.push(numbers[i] - numbers[i - 1]);
+  }
+  return seq;
+}
