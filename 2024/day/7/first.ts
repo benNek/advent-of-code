4b058e15c03bb8307419054170e7559318f88913
Benas Nekrošius <benasnek@gmail.com>
2024-12-07 00:14:22 -0500

    Solve day 7 part 1

---
+++ 2024/day/7/first.ts
@@ -0,0 +1,44 @@
+import getLines from "../../../helpers/readFile.ts";
+
+const lines = await getLines();
+
+let sum = 0;
+for (const line of lines) {
+    const target = parseInt(line.split(':')[0], 10);
+    const nums = line.split(':')[1].split(' ').filter(x => !!x).map(x => parseInt(x, 10));
+
+    let combos: string[][] = [];
+    generateOperators(nums.length - 1, [], combos);
+
+
+    for (const combo of combos) {
+        let result = nums[0];
+
+        let item = 1;
+        for (const operation of combo) {
+            if (operation === '*') {
+                result *= nums[item]
+            } else {
+                result += nums[item]
+            }
+            item++;
+        }
+
+        if (result === target) {
+            sum += target;
+            break;
+        }
+    }
+}
+
+console.log(sum);
+
+function generateOperators(length: number, operators: string[], result: string[][]) {
+    if (length === 0) {
+        result.push(operators);
+        return;
+    }
+
+    generateOperators(length - 1, [...operators, "+"], result)
+    generateOperators(length - 1, [...operators, "*"], result)
+}
