69e64297a3e3d9c8618177c223c77a8950498ee5
Benas Nekrošius <benasnek@gmail.com>
2024-12-13 16:14:01 +0100

    Solve day 13 part 1

---
+++ 2024/day/13/first.ts
@@ -0,0 +1,53 @@
+import getLines from "../../../helpers/readFile.ts";
+
+const INSTRUCTION_REGEX = /Button (.+): X\+(\d+), Y\+(\d+)/;
+const TARGET_REGEX = /Prize: X=(\d+), Y=(\d+)/;
+
+// i see what's coming for second part but fuck it, lets brute force it for the memes
+
+const lines = await getLines();
+
+let price = 0;
+for (let i = 0; i < lines.length; i += 4) {
+    const a = lines[i].match(INSTRUCTION_REGEX);
+    const ax = parseInt(a?.[2]!);
+    const ay = parseInt(a?.[3]!);
+
+    const b = lines[i + 1].match(INSTRUCTION_REGEX);
+    const bx = parseInt(b?.[2]!);
+    const by = parseInt(b?.[3]!);
+
+    const target = lines[i + 2].match(TARGET_REGEX);
+    const tx = parseInt(target?.[1]!);
+    const ty = parseInt(target?.[2]!);
+
+    const [aClicks, bClicks] = solve(ax, ay, bx, by, tx, ty);
+
+    if (aClicks === -1 || bClicks === -1) {
+        continue;
+    }
+
+    if (aClicks > 100 || bClicks > 100) {
+        console.log('exceeded 100 clicks')
+        continue;
+    }
+
+    // console.log(aClicks, bClicks);
+    price += aClicks * 3 + bClicks;
+}
+console.log("Total price is ", price);
+
+function solve(ax: number, ay: number, bx: number, by: number, tx: number, ty: number): [number, number] {
+    const clicksB = (ax * ty - ay * tx) / (ax * by - bx * ay);
+    const clicksA = (tx - bx * clicksB) / ax;
+
+    if (!looksLikeAnswer(clicksA) || !looksLikeAnswer(clicksB)) {
+        return [-1, -1];
+    }
+
+    return [clicksA, clicksB];
+}
+
+function looksLikeAnswer(x: number) {
+    return x % 1 === 0;
+}
