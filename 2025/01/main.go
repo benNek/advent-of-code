58384b335c61901870022f77ca8e570a222a4489
Benas Nekrošius <benasnek@gmail.com>
2025-12-01 10:28:14 +0200

    Solve day1 (with go!)

---
+++ 2025/01/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	part1(input)
+}
+
+func part1(input string) int {
+	zeroes := 0
+	position := 50
+	for _, line := range parseInput(input) {
+		dir := line[:1]
+		diff, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
+
+		if dir == "L" {
+			position -= diff
+		} else {
+			position += diff
+		}
+		position = mod(position, 100)
+
+		if position == 0 {
+			zeroes++
+		}
+	}
+	return zeroes
+}
+
+func part2(input string) int {
+	ans := 0
+	position := 50
+	// whooop learning how to put stuff in map in golang now
+	dirStep := map[byte]int{
+		'R': 1,
+		'L': -1,
+	}
+	for _, line := range parseInput(input) {
+		step, _ := dirStep[line[0]]
+		diff, _ := strconv.Atoi(line[1:])
+		for range diff {
+			position += step
+			if position%100 == 0 {
+				ans++
+			}
+		}
+	}
+	return ans
+}
+
+func parseInput(input string) []string {
+	return strings.Split(input, "\n")
+}
+
+func mod(a, b int) int {
+	return (a%b + b) % b
+}
---
+++ 2025/01/main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import "testing"
+
+var example = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
+
+func Test_part1(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "example",
+			input: example,
+			want:  3,
+		},
+		{
+			name:  "actual",
+			input: input,
+			want:  1158,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.input); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "example",
+			input: example,
+			want:  6,
+		},
+		{
+			name:  "actual",
+			input: input,
+			want:  6860,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.input); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
